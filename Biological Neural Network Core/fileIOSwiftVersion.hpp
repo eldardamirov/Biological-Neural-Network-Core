c010d7d01ad0dbce4566841796d3ad6a6300ca56
eldardamirov <eldardamirov75@mail.ru>
2018-10-11 00:10:40 +0300

    C++ fileIO methods basic implementation.

---
+++ Biological Neural Network Core/fileIOSwiftVersion.hpp
@@ -0,0 +1,47 @@
+//
+//  fileIOSwiftVersion.hpp
+//  Biological Neural Network Core
+//
+//  Created by Эльдар Дамиров on 10.10.2018.
+//  Copyright © 2018 Эльдар Дамиров. All rights reserved.
+//
+
+#ifndef fileIOSwiftVersion_hpp
+#define fileIOSwiftVersion_hpp
+
+#ifdef __cplusplus
+extern "C"
+    {
+#endif
+
+
+
+#include <sys/uio.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+//#include <cstdlib>
+#include <stdlib.h>
+#include <stdio.h>
+//#include <string>
+//#include <bitset>
+//#include <vector>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include <time.h>
+
+
+
+
+
+
+#ifdef __cplusplus
+    }
+#endif
+
+
+#endif /* fileIOSwiftVersion_hpp */
