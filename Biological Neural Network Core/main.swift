6b59b98921eff3a9f8762e393b1526024d5e6f26
eldardamirov <eldardamirov75@mail.ru>
2018-10-10 20:55:41 +0300

    Initial Commit

---
+++ Biological Neural Network Core/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Biological Neural Network Core
+//
+//  Created by Эльдар Дамиров on 10.10.2018.
+//  Copyright © 2018 Эльдар Дамиров. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
